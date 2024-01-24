f02059593ae45dcde2bbad900dd4f2f4ad8ee0d1
Senakara <106998274+sena-kara@users.noreply.github.com>
2024-01-24 21:48:04 +0300

    Add files via upload
    Word sorting with Python

---
+++ kelimeSiralama.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+cumle = "bugün hava çok güzel"
+
+# kelimeleri sırayla alt alta sırala
+
+kelimeler = cumle.split()
+kelimeler.sort()
+print(kelimeler)
+# for kelime in kelimeler:
+#     print(kelime)
+    
